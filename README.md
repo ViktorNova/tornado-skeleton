d48ec3f7ab4d4ad61c619c7936b3b597518ceb40
gregory80 <gregory.tomlinson@gmail.com>
2013-02-23 04:55:16 -0500

    basic bash script to skeleton

---
+++ README.md
@@ -0,0 +1,70 @@
+
+
+Heroku Skeleton
+-------------
+
+This is inspired by Mike Dory's work
+https://github.com/mikedory/Tornado-Heroku-Quickstart
+
+This is a bash script. Relies on 
+virtual env, which Heroku explicitly
+names in their documentation.
+
+Usage
+------
+
+How to build your environment.
+
+  bash build_env.sh ~/tmp/path/to/appdir
+
+This script is destructive. Use on clean directories.
+
+
+Structure
+---------
+
+Installs the following python
+packages to virtual env
+
+  tornado
+  gunicorn
+  redis
+  pylibmc
+  boto
+
+Builds the following directory
+and file structure
+
+app/
+    static/
+        js/
+          jquery-1.9.1.js
+          newT.js
+        css/
+        graphics/
+    scripts/
+    templates/
+        main.html
+venv/
+Procfile
+requirements.txt
+
+
+
+Activate virtual env
+--------------
+
+  source venv/bin/activate
+
+
+Additional Information
+------------------
+
+boto is added for ec2 / s3 support. Redis
+and Memcached are added as well as gunicorn
+
+pip freeze > requirements.txt builds the 
+virtual enviroment.
+
+Procfile is setup to be controlled via ruby's Foreman
+
---
+++ build_env.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+#
+#
+# author: gregory tomlinson
+# Liscense MIT
+# 
+# 
+# this was / is strongly influenced by
+# the great work by mike dory
+# https://github.com/mikedory/Tornado-Heroku-Quickstart
+#
+# this is customized for my "style" of app format
+# Assumes ruby Foreman is installed, adds
+# ec2 support via boto
+#
+# Use Foreman to manage the server process
+# via gunicorn
+# 
+# gunicorn for heroku start code via 
+# https://github.com/mccutchen
+if [ $# -lt 1 ]; then
+  echo "APP folder path required"
+  exit 0
+fi
+
+echo "Creating application $1"
+sleep 1
+mkdir -p $1
+pushd $1
+
+mkdir -p app/scripts app/static
+pushd app
+# in the app folder
+touch webapp.py __init__.py
+mkdir -p static/js static/css static/graphics templates
+cd static/js
+echo "Get static JS files"
+curl -O http://code.jquery.com/jquery-1.9.1.js
+curl -O https://raw.github.com/jeffreytierney/newT/master/newT.js
+popd
+
+touch README.md requirements.txt .gitignore .env
+virtualenv venv --distribute
+source venv/bin/activate
+
+# install some tornado packages
+pip install tornado
+pip install gunicorn
+pip install redis
+pip install pylibmc
+pip install boto #ec2 / s3 connector
+
+pip freeze > requirements.txt
+
+# make virtual env
+
+echo "This is a stub, for tornado on heroku" > README.md
+echo "*.pyc" >> .gitignore
+echo ".DS_Store" >> .gitignore
+echo ".env" >> .gitignore
+echo "venv/" >> .gitignore
+
+echo 'ENV="dev"' >> .env
+echo 'PORT=5000' >> .env
+echo 'MEMCACHE_SERVERS="127.0.0.1"' >> .env
+
+echo "web: gunicorn -k tornado --workers=4 --bind=0.0.0.0:$PORT 'app.webapp:webapp()'" > Procfile
+
+
+VAR1=$(cat <<EOF
+import tornado
+import tornado.options
+import tornado.web
+import os.path
+import sys
+import time
+import os
+import redis
+import pylibmc
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+class BasicHandler(tornado.web.RequestHandler):
+
+    @property
+    def db(self):
+        return self.application.db
+    def render(self, *args, **kwargs):
+        
+        kwargs.update({
+            "autoescape":None,
+            })
+        self.set_header("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate")
+        self.set_header("Pragma", "no-cache")
+        if 'chromeframe' in self.request.headers.get('User-Agent', []):
+            self.set_header("X-UA-Compatible","chrome=1")
+        return super(BasicHTMLHandler, self).render(*args, **kwargs)
+
+class MainHandler(BasicHandler):
+    def get(self):
+        self.finish("I am the main")
+
+class Application(tornado.web.Application):
+
+    def __init__(self):
+        handlers = [
+                (r"^/$", MainHandler),
+                ]
+        settings = dict(
+            cookie_secret="CHANGEMEPL3Z",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+            xheaders=True,
+            autoescape=None,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+def webapp():
+    app = Application()
+    return app
+
+EOF
+)
+
+
+VAR2=$(cat <<EOF
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+  <title>{{page_title}}</title>
+  
+</head>
+<body>
+  This is the main
+
+<script src="{{static_url("js/jquery-1.9.1.js")}}" type="text/javascript" charset="utf-8"></script>
+<script src="{{static_url("js/newT.js")}}" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" charset="utf-8">
+var _example_var = {{json_encode({"foo":"bar"})}}
+</script>
+</body>
+</html>
+
+EOF
+)
+echo "${VAR1}" > webapp.py
+echo "${VAR2}" > templates/main.html
+# leave the app/ dir
+popd
+
+# make a new Heroku app (or don't)
+echo "Do you want to start a Heroku app as well?"
+select yn in "y" "n"; do
+    case $yn in
+        y ) ./heroku.sh; break;;
+        n ) echo "run heroku.sh for new heroku apps"; break;;
+    esac
+done
+
+# no need for this file anymore, eh?
+#echo "Cleaning up..."
+#rm ./$0
+
+exit 0
+
