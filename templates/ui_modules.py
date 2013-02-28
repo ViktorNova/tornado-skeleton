dee461aa0a18224ead5328960c20c3922d05d560
gregory80 <gregory.tomlinson@gmail.com>
2013-02-28 08:47:38 -0500

    reshuffle folders

--- templates/ui_modules.py
+++ templates/ui_modules.py
@@ -5,5 +5,10 @@ class BasicModule(tornado.web.UIModule):
 	def _foo(self):
 		pass
 
-
+class JSFiles(BasicModule):
+	def render(self, js_files):
+		return self.render_string( "uimodules/scripttag.html", 
+			js_files=js_files, 
+			current_env=os.environ.get("ENV"),
+			compiled_name="compiled" )
 		
